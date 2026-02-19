099b7163a2be829a53746b15cd1427b33d20d90f
Link4Electronics <link4electronics@gmail.com>
2026-02-19 01:36:27 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/64x64/apps/knights.png
 export DESKTOP=/usr/share/applications/org.kde.knights.desktop
+export STARTUPWMCLASS=org.kde.knights
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
